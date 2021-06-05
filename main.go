b18750b6b9451fa47650c99ff45d029d0e503fb6
leiwen <657106593@qq.com>
2021-06-05 18:45:17 +0800

    feat:新增证书双向验证

--- main.go
+++ main.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"grpc-cli/services"
+	"io/ioutil"
 	"log"
 )
 
 func main(){
 	//客户端加载证书
-	creds, err := credentials.NewClientTLSFromFile("keys/server.crt", "leixiaotian")
+	//creds, err := credentials.NewClientTLSFromFile("keys/server.crt", "leixiaotian")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	//客户端加载证书,双向验证
+	cert, _ := tls.LoadX509KeyPair("cert/client.pem", "cert/client.key")
+	certPool := x509.NewCertPool()
+	ca, _ := ioutil.ReadFile("cert/ca.pem")
+	certPool.AppendCertsFromPEM(ca)
+
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: 				 []tls.Certificate{cert}, //客户端证书
+		ServerName:                  "localhost",
+		RootCAs:                     certPool,
+	})
+
 	//conn, err := grpc.Dial(":8081", grpc.WithInsecure())		//无证书连接
 	conn, err := grpc.Dial(":8081", grpc.WithTransportCredentials(creds))
 	if err != nil {

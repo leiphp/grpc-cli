28ec7b7c6cdfa07998c873fed21c254bfe1b746f
leiwen <657106593@qq.com>
2021-06-06 00:55:33 +0800

    feat:新增ca证书双向验证和grpc商品列表方法

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.5.2
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
 	google.golang.org/grpc v1.38.0
 	google.golang.org/protobuf v1.26.0
 )
--- main.go
+++ main.go
@@ -2,45 +2,33 @@ package main
 
 import (
 	"context"
-	"crypto/tls"
-	"crypto/x509"
 	"fmt"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
+	"grpc-cli/helper"
 	"grpc-cli/services"
-	"io/ioutil"
 	"log"
 )
 
 func main(){
-	//客户端加载证书
-	//creds, err := credentials.NewClientTLSFromFile("keys/server.crt", "leixiaotian")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	//客户端加载证书,双向验证
-	cert, _ := tls.LoadX509KeyPair("cert/client.pem", "cert/client.key")
-	certPool := x509.NewCertPool()
-	ca, _ := ioutil.ReadFile("cert/ca.pem")
-	certPool.AppendCertsFromPEM(ca)
-
-	creds := credentials.NewTLS(&tls.Config{
-		Certificates: 				 []tls.Certificate{cert}, //客户端证书
-		ServerName:                  "localhost",
-		RootCAs:                     certPool,
-	})
-
 	//conn, err := grpc.Dial(":8081", grpc.WithInsecure())		//无证书连接
-	conn, err := grpc.Dial(":8081", grpc.WithTransportCredentials(creds))
+	conn, err := grpc.Dial(":8081", grpc.WithTransportCredentials(helper.GetClientCreds()))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
+
 	goodsClient := services.NewGoodsServiceClient(conn)
-	goodsRes, err := goodsClient.GetGoodsStock(context.Background(),&services.GoodsRequest{GoodsId:12})
+	//获取单个商品
+	//goodsRes, err := goodsClient.GetGoodsStock(context.Background(),&services.GoodsRequest{GoodsId:12})
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println(goodsRes.GoodsStock)
+
+	//获取多个商品
+	response, err := goodsClient.GetGoodsStocks(context.Background(), &services.GoodsSize{Size:10})
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(goodsRes.GoodsStock)
+	fmt.Println(response.Goodsres)
 }

ca24f245fc6b8206ec40dffc5c3bbec676c31c82
leiwen <657106593@qq.com>
2021-06-06 12:17:43 +0800

    feat:新增订单client调用

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/golang/protobuf/ptypes/timestamp"
 	"google.golang.org/grpc"
 	"grpc-cli/helper"
 	"grpc-cli/services"
 	"log"
+	"time"
 )
 
 func main(){
@@ -17,18 +19,31 @@ func main(){
 	}
 	defer conn.Close()
 
+	//商品client
 	goodsClient := services.NewGoodsServiceClient(conn)
-	//获取单个商品
-	//goodsRes, err := goodsClient.GetGoodsStock(context.Background(),&services.GoodsRequest{GoodsId:12})
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//fmt.Println(goodsRes.GoodsStock)
+	//订单client
+	ordersClient := services.NewOrdersServiceClient(conn)
 
-	//获取多个商品
-	response, err := goodsClient.GetGoodsStocks(context.Background(), &services.GoodsSize{Size:10})
+	//商品服务-获取单个商品
+	goodsRes, err := goodsClient.GetGoodsStock(context.Background(),&services.GoodsRequest{GoodsId:10,GoodsArea:services.GoodsAreas_B})
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(response.Goodsres)
+	fmt.Println(goodsRes.GoodsStock)
+
+	//商品服务-获取多个商品
+	//response, err := goodsClient.GetGoodsStocks(context.Background(), &services.GoodsSize{Size:10})
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println(response.Goodsres)
+
+	//订单服务-创建订单
+	t := timestamp.Timestamp{Seconds:time.Now().Unix()}
+	ordersRes, err := ordersClient.CreateOrder(context.Background(),&services.OrderMain{OrderId:1001,OrderNo:"0001",OrderMoney:90,OrderTime:&t})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ordersRes)
+
 }
